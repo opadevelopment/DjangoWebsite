3748ad702c7317176329cb2eed36a0f97dcab4f7
Samppa-code <60503282+Samppa-code@users.noreply.github.com>
2023-02-10 00:51:05 +0200

    Lisätty sivuston aiheille placeholder sivut

--- website/website/urls.py
+++ website/website/urls.py
@@ -4,5 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.hello, name='hello'),
     path('hello/<str:name>/', views.hello, name='hello'),
-    path('testi_sivu/', views.testi_sivu, name='testi_sivu')
+    path('testi_sivu/', views.testi_sivu, name='testi_sivu'),
+    path('opiskelija_sivu/',views.opiskelija_sivu, name='opiskelija_sivu'),
+    path('muutto_sivu/',views.muutto_sivu, name='muutto_sivu'),
+    path('tyoasia_sivu/',views.tyoasia_sivu, name='tyoasia_sivu'),
+    path('talous_sivu/',views.talous_sivu, name='talous_sivu'),
+    path('yhteydenotto_sivu/', views.yhteydenotto_sivu, name='yhteydenotto_sivu'),
+
+
 ]
--- website/website/views.py
+++ website/website/views.py
@@ -6,3 +6,18 @@ def hello(request, name='World'):
 
 def testi_sivu(request):
     return render(request,'testi_sivu.html')
+
+def opiskelija_sivu(request):
+    return render(request, 'opiskelija_sivu.html')
+
+def muutto_sivu(request):
+    return render(request,'muutto_sivu.html')
+
+def tyoasia_sivu(request):
+    return render(request, 'tyoasia_sivu.html')
+
+def talous_sivu(request):
+    return render(request, 'talous_sivu.html')
+
+def yhteydenotto_sivu(request):
+    return render(request, 'yhteydenotto_sivu.html')
