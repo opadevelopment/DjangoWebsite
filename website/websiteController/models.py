d05beba4626a64a1162828ba4cf92c44f03fca65
Samppa-code <60503282+Samppa-code@users.noreply.github.com>
2023-03-05 23:15:24 +0200

    Lisätty admin sivu

--- website/websiteController/models.py
+++ website/websiteController/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Palaute(models.Model):
+    nimi = models.CharField(max_length=50)
+    palaute = models.TextField(max_length=1000)
+    sposti = models.EmailField(max_length=150)
+
+    def __str__(self):
+        return self.palaute
--- website/websiteController/urls.py
+++ website/websiteController/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
 
 urlpatterns = [
+   # path('admin/', admin.site.urls),
     path('hello/', views.hello, name='hello'),
+    
 ]
