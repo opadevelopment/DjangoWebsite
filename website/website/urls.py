dac1cf304f85195bbd98edb9265355fdfb181873
Samppa-code <60503282+Samppa-code@users.noreply.github.com>
2023-03-07 22:41:56 +0200

    Siivottu koodia

--- website/website/urls.py
+++ website/website/urls.py
@@ -8,12 +8,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.hello, name='hello'),
     path('hello/<str:name>/', views.hello, name='hello'),
-    path('testi_sivu/', views.testi_sivu, name='testi_sivu'),
-    path('opiskelija_sivu/',views.opiskelija_sivu, name='opiskelija_sivu'),
-    path('muutto_sivu/',views.muutto_sivu, name='muutto_sivu'),
-    path('tyoasia_sivu/',views.tyoasia_sivu, name='tyoasia_sivu'),
-    path('talous_sivu/',views.talous_sivu, name='talous_sivu'),
-    path('yhteydenotto_sivu/', views.yhteydenotto_sivu, name='yhteydenotto_sivu'),
+    path('testi/', views.testi, name='testi'),
+    path('opiskelija/',views.opiskelija, name='opiskelija'),
+    path('muutto/',views.muutto, name='muutto'),
+    path('tyoasia/',views.tyoasia, name='tyoasia'),
+    path('talous/',views.talous, name='talous'),
+    path('palaute/', views.palaute, name='palaute'),
 
 
 ]
